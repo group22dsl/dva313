8f27d927175c897663ff9b3dec2a9484082a7f54
Nuwan Darshika <nu1darshika@gmail.com>
2022-12-04 23:51:38 +0530

    Added Intent filters according to the settings

--- app/src/main/java/com/example/myapplication/MyBroadcastReceiver.java
+++ app/src/main/java/com/example/myapplication/MyBroadcastReceiver.java
@@ -3,13 +3,19 @@ package com.example.myapplication;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.sql.Array;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
     public String collectedData;
@@ -20,7 +26,21 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
         String[] myStrings = collectedData.split("@");
         JSONObject jsonObject = new JSONObject();
 
+
         try {
+            // THIS IS SAMPLE
+            SharedPreferences sharedpreferences = context.getSharedPreferences("appSettings", Context.MODE_PRIVATE);
+            Set<String> whiteListApps = new HashSet<String>();
+            whiteListApps = sharedpreferences.getStringSet("whiteList",null);
+
+            boolean contains = whiteListApps.contains(myStrings[0]);
+            if(contains == true){
+                Toast.makeText(context, "You got new message from whitelisted app" , Toast.LENGTH_SHORT ).show();
+            }
+            else{
+                Toast.makeText(context, "You got new message from blacklisted app" , Toast.LENGTH_SHORT ).show();
+            }
+
             // TODO: string sent MUST have this structure for parsing into JSON to work.
             jsonObject.put("Origin",myStrings[0]);
             //We use our function here to turn the given string to an actual date.

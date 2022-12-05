f2a6ea09af53987e11ac0dbc17ffec7d9d53f499
Armin Hrbat <92021852+arminhrbat@users.noreply.github.com>
2022-12-05 14:17:32 +0100

    setting up for merging

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -8,7 +11,9 @@ import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +21,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        MyBroadcastReceiver myBroadcastReceiver = new MyBroadcastReceiver();
+        IntentFilter myFilter = new IntentFilter("testData");
+
+        SharedPreferences sharedpreferences = getSharedPreferences("appSettings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+
+        //this should get from DB
+        String[] listOfExistingScores = {"com.example.a", "true", "Three"};
+        //end
+
+        Set<String> set = new HashSet<String>();
+        for(int l=0; l<listOfExistingScores.length; l++){
+            set.add(listOfExistingScores[l]);
+        }
+
+        editor.putStringSet("whiteList", set);
+
+        editor.commit(); // commit changes
+;    }
 
     public void onSelectTodo(View view){
         Intent intent = new Intent(MainActivity.this, TODOTaskActivity.class);

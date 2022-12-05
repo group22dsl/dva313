e8848b13331ed0387d948e3ca90487f1a1727e79
MartenStromBaverlind <msd20009@student.mdh.se>
2022-12-05 11:44:01 +0100

    setup firebase database and test function to send data

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,8 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
@@ -15,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     public ListView databaseListView;
     public static final String TASK_ID = "";
-
+    private DatabaseReference rootDatabaseref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         MyBroadcastReceiver myBroadcastReceiver = new MyBroadcastReceiver();
         IntentFilter myFilter = new IntentFilter("testData");
         registerReceiver(myBroadcastReceiver, myFilter);
+
+        rootDatabaseref = FirebaseDatabase.getInstance("https://dva313-5397d-default-rtdb.europe-west1.firebasedatabase.app/").getReference().child("logs");
 
         databaseListView = (ListView) findViewById(R.id.database_list);
 
@@ -46,7 +53,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void writeNewLog(View view){
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvxyz";
+        StringBuilder sb = new StringBuilder(10);
+        for (int i = 0; i < 10; i++) {
+            int index = (int)(AlphaNumericString.length() * Math.random());
+            sb.append(AlphaNumericString.charAt(index));
+        }
+        Toast.makeText(this,"Hello",Toast.LENGTH_LONG).show();
 
+        rootDatabaseref.child(String.valueOf(sb)).child("origin").setValue("test");
+        rootDatabaseref.child(String.valueOf(sb)).child("date").setValue("randomdate");
+        rootDatabaseref.child(String.valueOf(sb)).child("message").setValue("something something");
+        rootDatabaseref.child(String.valueOf(sb)).child("category").setValue("randomstatus");
+    }
 
     public void onAddClick(View view)
     {

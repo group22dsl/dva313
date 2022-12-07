202d45421493277d8002b8a9c03b017f28aeeba3
MartenStromBaverlind <msd20009@student.mdh.se>
2022-12-07 15:49:44 +0100

    test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,26 +1,84 @@
 package com.example.myapplication;
 
+import static android.os.PersistableBundle.readFromStream;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.net.Uri;
+
+import android.os.Build;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+import android.os.Handler;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.owncloud.android.lib.common.OwnCloudClient;
+import com.owncloud.android.lib.common.OwnCloudClientFactory;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.Timer;
+import java.util.logging.LogRecord;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Credentials;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //  TODO CREATE THREAD, TEST TIMER 5SECONDS DONE !
+        //  TODO PARSE JSON FILE INTO DBOBJECT
+        //  TODO CHANGE SETTING WITH FUNCTION
+
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try  {
+                    request();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
 
         MyBroadcastReceiver myBroadcastReceiver = new MyBroadcastReceiver();
         IntentFilter myFilter = new IntentFilter("testData");
@@ -42,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
         editor.commit(); // commit changes
 ;    }
 
+
     public void onSelectTodo(View view){
         Intent intent = new Intent(MainActivity.this, TODOTaskActivity.class);
         startActivity(intent);
@@ -50,6 +109,39 @@ public class MainActivity extends AppCompatActivity {
     public void onSelectAPPFilters(View view){
         Intent intent = new Intent(MainActivity.this, APPFiltersActivity.class);
         startActivity(intent);
+    }
+    public void request() throws IOException{
+        OkHttpClient client = new OkHttpClient();
+        String credential = Credentials.basic("Marten","Mårten Pass Pot Cloud 789!");
+        Request request = new Request.Builder()
+                .addHeader("Authorization",credential)
+                .url("https://nextcloud.thepotatoservices.com/apps/files/?dir=/&openfile=19693")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+                Log.d("Error", "Error");
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    ResponseBody responseBody = response.body();
+                    String resStr = response.body().string();
+                    try {
+                        JSONObject json = new JSONObject(resStr);
+                        json.toString();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    Log.v("tetete","test");
+                    //TODO save into dbobject and then call updateDB(DBobject)
+                }
+            }
+        });
     }
 
 }

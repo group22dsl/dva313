fe238a81615b333b4099daaa381781a49b0b4b05
MartenStromBaverlind <msd20009@student.mdh.se>
2022-12-09 16:08:47 +0100

    added send json file to nextcloud

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONException;
@@ -21,12 +22,21 @@ import java.util.Set;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.Credentials;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
+import okio.BufferedSink;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private OkHttpClient client;
+    private Request request;
 
     @SuppressLint("NewApi")
     @Override
@@ -39,12 +49,14 @@ public class MainActivity extends AppCompatActivity {
         //  TODO PARSE JSON FILE INTO DBOBJECT
         //  TODO CHANGE SETTING WITH FUNCTION
 
+
         Thread thread = new Thread(new Runnable() {
 
             @Override
             public void run() {
-                try  {
-                    requestConfiguration();
+                try {
+                    //requestConfiguration();
+                    sendAnalytics();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -64,27 +76,29 @@ public class MainActivity extends AppCompatActivity {
         //end
 
         Set<String> set = new HashSet<String>();
-        for(int l=0; l<listOfExistingScores.length; l++){
+        for (int l = 0; l < listOfExistingScores.length; l++) {
             set.add(listOfExistingScores[l]);
         }
 
         editor.putStringSet("whiteList", set);
 
         editor.commit(); // commit changes
-;    }
+        ;
+    }
 
-    public void onSelectTodo(View view){
+    public void onSelectTodo(View view) {
         Intent intent = new Intent(MainActivity.this, TODOTaskActivity.class);
         startActivity(intent);
     }
 
-    public void onSelectAPPFilters(View view){
+    public void onSelectAPPFilters(View view) {
         Intent intent = new Intent(MainActivity.this, APPFiltersActivity.class);
         startActivity(intent);
     }
-    public void requestConfiguration() throws IOException{
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
+
+    public void requestConfiguration() throws IOException {
+        client = new OkHttpClient();
+        request = new Request.Builder()
                 .url("https://nextcloud.thepotatoservices.com/s/mGaCFbGynG7cLq7/download/test.json")
                 .build();
 
@@ -94,12 +108,13 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
                 Log.d("HTTP_REQUEST", "HTTP request failed, Error: " + e);
             }
+
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
                 String jsonStr = response.body().string();
                 try {
                     JSONObject data = new JSONObject(jsonStr);
-                    Log.v("HTTP_REQUEST", "Json Created, contents: " + data);
+                    Log.v("HTTP_REQUEST", "Json Created, contents: " + data); // TODO GET JSON FILE SETTINGS - FIXED
                 } catch (JSONException e) {
                     Log.d("HTTP_REQUEST", "HTTP Request error: " + e);
                     Log.d("HTTP_REQUEST", "HTTP result was: " + jsonStr);
@@ -109,5 +124,29 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void sendAnalytics() throws IOException, JSONException {
+        String user = "Marten";
+        String pass = "Mårten Pass Pot Cloud 789!";
+        String credential = Credentials.basic(user,pass);
+        OkHttpClient client = new OkHttpClient();
+        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+        JSONObject json = new JSONObject();
+        json.put("title","test");
+        json.put("name","plplpl");
 
+
+        RequestBody body = RequestBody.create(json.toString(), JSON);
+        Request request = new Request.Builder()
+                .url("https://nextcloud.thepotatoservices.com/s/9AnjP4CSx4HpJYd")
+                .addHeader("Authorization",credential)
+               // .url("https://teeee.free.beeceptor.com")
+                .post(body)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        Log.v("response",response.toString());
+
+
+    }
 }

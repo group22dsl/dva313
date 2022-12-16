4cdb395bd7623a5d32ce4d4c26396a7f98f37fb9
VictorKnutmejer <118001993+VictorKnutmejer@users.noreply.github.com>
2022-12-16 17:49:03 +0100

    Crashes when using the DAO's
    Crashes when using the DAO's

--- app/src/main/java/com/example/myapplication/MyBroadcastReceiver.java
+++ app/src/main/java/com/example/myapplication/MyBroadcastReceiver.java
@@ -5,12 +5,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.JsonReader;
+import android.util.Log;
 import android.widget.Toast;
+
+import androidx.room.Dao;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
@@ -18,15 +22,17 @@ import java.util.Set;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
     public String collectedData;
-    CacheDAO cacheDAO;
-    WhitelistDAO whitelistDAO;
-    cacheEntry entry;
+    public CacheDAO cacheDAO;
+    public WhitelistDAO whitelistDAO;
+    private cacheEntry entry;
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
         collectedData = intent.getStringExtra("data");
         String ID;
+
+        boolean existsInWhitelist = false;
         try {
             JSONObject jsonObject = new JSONObject(collectedData);
             ID=jsonObject.get("ID").toString();
@@ -34,24 +40,24 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
             SharedPreferences sharedpreferences = context.getSharedPreferences("appSettings", Context.MODE_PRIVATE);
             Set<String> whiteListApps = new HashSet<String>();
             //whiteListApps = sharedpreferences.getStringSet("whiteList",null);
-            List<whitelistEntry> testWhitelist = whitelistDAO.getEntireWhitelist();
-            //index 2 is where ID is placed.
-            //boolean contains = whiteListApps.contains(ID);
-            for (int i = 0; ; i++)
-            if(contains == true){
-                Toast.makeText(context, "You got new message from whitelisted app" , Toast.LENGTH_SHORT ).show();
-                Toast.makeText(context, collectedData, Toast.LENGTH_LONG).show();
-                entry = new cacheEntry(ID, jsonObject.get("Priority").toString(), jsonObject.get("Data").toString());
-                cacheDAO.addCacheEntry(entry);
+            //TODO is the error because of this list or because of what is sent from database?
+            whitelistDAO.getEntireWhitelist().size();
+            for (int i = 0; whitelistDAO.getEntireWhitelist().size() > i; i++){
+                if(whitelistDAO.getEntireWhitelist().get(i).getID() == ID){
+                    Toast.makeText(context, "You got new message from whitelisted app" , Toast.LENGTH_SHORT ).show();
+                    Toast.makeText(context, collectedData, Toast.LENGTH_LONG).show();
+                    entry = new cacheEntry(ID, jsonObject.get("Priority").toString(), jsonObject.get("Data").toString());
+                    cacheDAO.addCacheEntry(entry);
+                    existsInWhitelist = true;
+                    break;
+                }
             }
-            else{
+            if (!existsInWhitelist){
                 Toast.makeText(context, "You got new message from blacklisted app" , Toast.LENGTH_SHORT ).show();
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
-
     }
     //This function converts string to actual date.
     private String convertToDate(String stringDate) {
